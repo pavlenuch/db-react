2f45a6204bae1d23116e5c847cfa8e933af196f6
Павленко Александр Алексеевич <pavlenko@galaktika.local>
2021-01-13 18:28:13 +0300

    Updates

---
+++ precache-manifest.d45d1aad71609fde7ff63dbff132d460.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c668f771cd621cb1fae5588d5e77ab",
+    "url": "/db-react/index.html"
+  },
+  {
+    "revision": "c69bcac4acd5046b3a1f",
+    "url": "/db-react/static/css/main.18b210b4.chunk.css"
+  },
+  {
+    "revision": "e12ef663b7e8a0553d15",
+    "url": "/db-react/static/js/2.54608119.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/db-react/static/js/2.54608119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c69bcac4acd5046b3a1f",
+    "url": "/db-react/static/js/main.0c046a3b.chunk.js"
+  },
+  {
+    "revision": "1d17dd2ea145a5d614b3",
+    "url": "/db-react/static/js/runtime-main.d0dd2fe2.js"
+  }
+]);
